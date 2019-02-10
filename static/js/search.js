cb5e6479c2943506cbdbcc3a50f8d5d670936aec
RC Pery <rcpery@tutanota.com>
2019-02-10 19:09:09 +0800

    added get-type to changed default value of #search-type

--- static/js/search.js
+++ static/js/search.js
@@ -1,7 +1,13 @@
+// vars
+
 var prev = document.getElementById("prev");
 var next = document.getElementById("next");
-
+var get_type = JSON.parse(document.getElementById("get-type").innerText);
+var set_type = document.getElementById("search-type");
 var offset_data = JSON.parse(document.getElementById("offset-data").innerText);
+
+console.log(get_type);
+console.log(set_type.value);
 
 if(offset_data.prev_offset < 0){
     prev.style.display = "none";
@@ -19,4 +25,12 @@ else{
     next.style.display = "inline";
 }
 
-document.getElementById("showcase").removeChild("#offset-data");
+for (var i, j = 0; i = set_type.options[j]; j++){
+	if(i.value == get_type.type){
+		set_type.selectedIndex = j;
+		break;
+	}
+}
+
+// document.getElementById("showcase").removeChild(document.getElementById("offset-data"));
+
